0154c68fd68b07a21f148095fbcf674309554742
Ksenya <ksyuhacr7@gmail.com>
2022-12-17 17:45:31 +0300

    initial commit

---
+++ SumOfMultipleInMassive/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] array = new int [] {4, 60, 2, 15, 1, 9};
+int sum = 0; // сумма произведений
+for (int i = 0; i < array.Length / 2; i++)
+{
+    sum = sum + array[i] * array [array.Length - 1 - i];
+    Console.WriteLine(sum);
+}
+Console.WriteLine(sum); //126
