0efaa8cf2886c65ab26050a55e6330a6869e4835
Ksenya <ksyuhacr7@gmail.com>
2022-12-17 18:11:25 +0300

    + number 2 and 3

---
+++ HowManyPlusNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите значение");
+int M1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение");
+int M2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение");
+int M3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение");
+int M4 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение");
+int M5 = Convert.ToInt32(Console.ReadLine());
+int count = 0; //счетчик положительных чисел
+if (M1 > 0)
+{
+    count = count + 1;
+}
+if (M2 > 0)
+{
+    count = count + 1;
+}
+if (M3 > 0)
+{
+    count = count + 1;
+}
+if (M4 > 0)
+{
+    count = count + 1;
+}
+if (M5 > 0)
+{
+    count = count + 1;
+}
+Console.WriteLine(count);
---
+++ LineEquation/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите значение b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = (b2-b1) / (k1-k2); // преобразовано из системы уравнений
+double y = k1 * x + b1;
+Console.WriteLine("(" + x + "; " + y + ")");
